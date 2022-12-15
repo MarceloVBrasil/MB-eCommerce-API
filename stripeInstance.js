ce9298a40c0ddfe43e1acaaec94ee004abdbdaaa
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-12-15 13:49:31 -0300

    use env variable for stripe

--- stripeInstance.js
+++ stripeInstance.js
@@ -1,1 +1,1 @@
-exports.stripeInstance = require("stripe")(("sk_test_51M3lWoBv6xxhS7gsfIUIgtVQf5to2Z1YHLbLH7RtoRnJgikAtapiqBwofni4UrZVF3XSDNqgvGfwgEnG0cbWReZu0002X32Wi8"))
+exports.stripeInstance = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
